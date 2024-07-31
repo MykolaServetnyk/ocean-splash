f1e335789428628e3e1d35f2ea08c708c1563541
MykolaServetnyk <mikokobass@gmail.com>
2024-07-31 20:14:06 +0300

    main added

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -1,21 +1,13 @@
 (() => {
   const menuBtn = document.querySelector("[data-menu-button]");
   const mobileMenu = document.querySelector("[data-menu]");
-  const headerLogo = document.querySelector(".header-logo");
 
-  if (menuBtn && mobileMenu && headerLogo) {
+  if (menuBtn && mobileMenu) {
     menuBtn.addEventListener("click", () => {
       const isMenuOpen = menuBtn.getAttribute("aria-expanded") === "true" || false;
 
       menuBtn.classList.toggle("is-open");
       mobileMenu.classList.toggle("is-open");
-
-      if (isMenuOpen) {
-        headerLogo.style.display = 'block';
-      } else {
-        headerLogo.style.display = 'none';
-      }
-
       menuBtn.setAttribute("aria-expanded", !isMenuOpen);
     });
   }
