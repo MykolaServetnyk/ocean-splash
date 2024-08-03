56fb286d28b1905a8a8a5a45b0a948f0f16366ad
MykolaServetnyk <mikokobass@gmail.com>
2024-08-03 22:20:37 +0300

    mobile-menu fixed

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -1,6 +1,7 @@
 (() => {
   const menuBtn = document.querySelector("[data-menu-button]");
   const mobileMenu = document.querySelector("[data-menu]");
+  const mobileNavLinks = document.querySelectorAll(".mobile-nav-link a");
 
   if (menuBtn && mobileMenu) {
     menuBtn.addEventListener("click", () => {
@@ -11,4 +12,14 @@
       menuBtn.setAttribute("aria-expanded", !isMenuOpen);
     });
   }
+
+  mobileNavLinks.forEach(link => {
+    link.addEventListener("click", () => {
+      if (mobileMenu.classList.contains("is-open")) {
+        menuBtn.classList.remove("is-open");
+        mobileMenu.classList.remove("is-open");
+        menuBtn.setAttribute("aria-expanded", false);
+      }
+    });
+  });
 })();
--- src/main.js
+++ src/main.js
@@ -1,4 +1,3 @@
 import './js/mobile-menu';
-import './js/modal';
 import './js/faq';
 import './js/gallery';
