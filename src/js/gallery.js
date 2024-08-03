61f2b137d4aa302b67b308634a35f36d5d7735b9
MykolaServetnyk <mikokobass@gmail.com>
2024-08-03 20:11:27 +0300

    slider

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,12 +1,26 @@
-// import Swiper, { Navigation, Pagination } from 'swiper';
+'use strict'
+import Swiper from 'swiper';
+import { Navigation, Pagination, Keyboard, Mousewheel } from 'swiper/modules';
+import 'swiper/css';
 
-// (() => {
-//   const swiper = new Swiper('.swiper-container', {
-//     modules: [Navigation, Pagination],
-//     navigation: {
-//       nextEl: '.swiper-button-next',
-//       prevEl: '.swiper-button-prev',
-//     },
-//     loop: true,
-//   });
-// })();
+(() => {
+  const swiper = new Swiper('.swiper', {
+    centeredSlides: true,
+    spaceBetween: 100,
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+        centeredSlides: true,
+      },
+      1440: {
+        slidesPerView: 3,
+      }
+    },
+    modules: [Navigation, Pagination, Keyboard, Mousewheel],
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    loop: true,
+  });
+})();
