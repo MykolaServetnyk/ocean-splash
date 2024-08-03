ea8eb8eefc55f83f86d8c1d42a8c13335e66587b
Victor Petrenko <138097657+Victor-Lars-Petrenko@users.noreply.github.com>
2024-08-03 10:53:17 +0300

    footer & gallery

---
+++ src/js/gallery.js
@@ -0,0 +1,12 @@
+import Swiper, { Navigation, Pagination } from 'swiper';
+
+(() => {
+  const swiper = new Swiper('.swiper-container', {
+    modules: [Navigation, Pagination],
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    loop: true,
+  });
+})();
